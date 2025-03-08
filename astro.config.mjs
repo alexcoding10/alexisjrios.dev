52c6aefd683c21831cc1927c700a98e333d89d57
AlexCoding <alexcoding10@gmail.com>
2025-03-08 01:24:25 +0100

    Update changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/alexisjrios.dev/', // ← Mueve `base` aquí
   vite: {
     plugins: [tailwindcss()],
-    base:'/alexisjrios.dev/'
-  }
+  },
 });
